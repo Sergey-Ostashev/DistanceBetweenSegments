5343db422cf01d93f7c2d8e45d3886d659bd8962
Sergey Ostashev <ostashev.sergey@yandex.ru>
2022-06-13 14:59:18 +0300

    Code comments

--- dbs/dbs.cpp
+++ dbs/dbs.cpp
@@ -1,6 +1,7 @@
 // dbs.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <algorithm>
 #include <iostream>
 #include <functional>
 #include <vector>
@@ -9,85 +10,76 @@
 using namespace std;
 using namespace dbs;
 
-double distance(const Segment& s1, const Segment& s2, double& tr1, double& tr2)
+double distance(const Segment& s1, const Segment& s2)
 {
-	if (s1.is_point() && s2.is_point())
+	if (s1.is_point() && s2.is_point()) // trivial case: all segments are points
 		return (s2.m_end - s1.m_end).norm();
 	else
 	{
+		// this function returns square of distance between points of line segments. Each point represents by number t between 0 and 1,
+		// where 0 is start point of segment and 1 - end point. This function is quadratic with respect to variables t1 and t2. So finding 
+		// it's minimum is solving system of two linear equations.
 		auto f = [&s1, &s2](double t1, double t2) {
 			return (s2.m_start - s1.m_start + 
 				(s2.m_end - s2.m_start) * t2 -
 				(s1.m_end - s1.m_start) * t1).sqr_norm();
 		};
 
-		auto dfdt1 = [&s1, &s2](double t1, double t2) {
-			return (s2.m_start - s1.m_start) * (s1.m_end - s1.m_start) * -2 - (s2.m_end - s2.m_start) * (s1.m_end - s1.m_start) * 2 * t2 +
-				2 * t1 * (s1.m_end - s1.m_start).sqr_norm();
-		};
-
-		auto dfdt2 = [&s1, &s2](double t1, double t2) {
-			return (s2.m_start - s1.m_start) * (s2.m_end - s2.m_start) * 2 - (s2.m_end - s2.m_start) * (s1.m_end - s1.m_start) * 2 * t1 +
-				2 * t1 * (s1.m_end - s1.m_start).sqr_norm();
-		};
-
 		vector<double> possible_t1;
 		vector<double> possible_t2;
-		double inner_product = (s2.m_end - s2.m_start) * (s1.m_end - s1.m_start);
-		if (!s1.is_point() && !s2.is_point() && ((inner_product * inner_product) / (s1.sqr_len() * s2.sqr_len()) == 1))
-		{
-			double ts1, ts2;
-			double te1, te2;
-			Segment s2_start = { s2.m_start, s2.m_start };
-			Segment s2_end = { s2.m_end, s2.m_end };
-			double r1 = distance(s1, s2_start, ts1, ts2);
-			double r2 = distance(s1, s2_end, te1, te2);
 
-			if (((ts1 < 0) && (te1 < 0)) ||
-				((ts1 > 1) && (te1 > 1)))
+		// calculation coefficiets of system.
+		double A = (s2.m_start - s1.m_start) * (s2.m_end - s2.m_start) * 2;
+		double B = (s2.m_end - s2.m_start) * (s1.m_end - s1.m_start) * 2;
+		double C = 2 * s2.sqr_len();
+		double D = (s2.m_start - s1.m_start) * (s1.m_end - s1.m_start) * 2;
+		double E = B;
+		double F = 2 * s1.sqr_len();
+
+		// calculation of determinants.
+		double det = C * F - E * B;
+		double det1 = C * D - A * E;
+		double det2 = B * D - A * F;
+
+		if ((abs(det) < numeric_limits<double>::epsilon()) && !s1.is_point() && !s2.is_point())
+		{
+			// Determinant of coefficient matrix is zero, but both segments have non-zero length. This is a case of parallel segments.
+			// System has infinite number of solutions. But need to be sure that both values lie in interval [0, 1]. Otherwise, check only boundary values.
+			double ts1 = A / B;			// value of t1 corresponding to t2 = 0
+			double te1 = (C + A) / B;	// value of t1 corresponding to t2 = 1
+			if (((ts1 < 0) && (te1 < 0)) || ((ts1 > 1) && (te1 > 1)))
 			{
+				// ts1 and te1 lie on same side of first segment. So, there is no solution of system that is appropriate.
 				possible_t1 = { 0, 1 };
 				possible_t2 = { 0, 1 };
 			}
-			else
+			else if ((ts1 >= 0) && (ts1 <= 1))
 			{
-				tr1 = ts1;
-				tr2 = ts2;
-				return r1;
+				possible_t1.push_back(ts1);
+				possible_t2.push_back(0);
+			}
+			else 
+			{
+				possible_t1.push_back(te1);
+				possible_t2.push_back(1);
 			}
 		}
 		else
 		{
-			double D = (s2.m_start - s1.m_start) * (s1.m_end - s1.m_start) * 2;
-			double E = (s2.m_end - s2.m_start) * (s1.m_end - s1.m_start) * 2;
-			double F = 2 * s1.sqr_len();
-
-			double A = (s2.m_start - s1.m_start) * (s2.m_end - s2.m_start) * 2;
-			double B = (s2.m_end - s2.m_start) * (s1.m_end - s1.m_start) * 2;
-			double C = 2 * s2.sqr_len();
-
-			double det = C * F - E * B;
-			double det1 = C * D - A * E;
-			double det2 = B * D - A * F;
-
+			// solving system of equations. If one of segments is point system reduces to single equation.
 			double t1 = !s1.is_point() ? (!s2.is_point() ? det1 / det : D / F) : 0;
 			double t2 = !s2.is_point() ? (!s1.is_point() ? det2 / det : -A / C) : 0;
 
-			if ((t1 < 0) || (t1 > 1))
-			{
-				possible_t1.push_back(0);
-				possible_t1.push_back(1);
-			}
-			else
-				possible_t1.push_back(t1);
+			// Check that values lie in the interval [0, 1]. If it's false, use the nearest boundary value.
+#define	CHECK_AND_ADD_VALUE(v, vs)	if (v < 0)				\
+										vs.push_back(0);	\
+									else if (v > 1)			\
+										vs.push_back(1);	\
+									else					\
+										vs.push_back(v);
 
-			if ((t2 < 0) || (t2 > 1))
-			{
-				possible_t2.push_back(0);
-				possible_t2.push_back(1);
-			}
-			else
-				possible_t2.push_back(t2);
+			CHECK_AND_ADD_VALUE(t1, possible_t1)
+			CHECK_AND_ADD_VALUE(t2, possible_t2)
 		}
 
 		double min_d = numeric_limits<double>::max();
@@ -96,24 +88,19 @@ double distance(const Segment& s1, const Segment& s2, double& tr1, double& tr2)
 		{
 			for (double pt2 : possible_t2)
 			{
-				double d = sqrt(f(pt1, pt2));
+				double d = f(pt1, pt2);
 				if (d < min_d)
-				{
-					tr1 = pt1;
-					tr2 = pt2;
 					min_d = d;
-				}
 			}
 		}
 
-		return min_d;
+		return sqrt(min_d);
 	}
 }
 
 void test(const Segment& s1, const Segment& s2, double etalon)
 {
-	double t1, t2;
-	double d = distance(s1, s2, t1, t2);
+	double d = distance(s1, s2);
 	if (d != etalon)
 		cerr << "Error: answer=" << d << ", " << "etalon=" << etalon << endl;
 	else
@@ -179,7 +166,7 @@ void test_all()
 	}
 
 	{
-		// two intersect segments
+		// two intersect perpendicular segments
 		Segment s1{ {0, 0, 0}, {3, 0, 0} };
 		Segment s2{ {0, 1, 0}, {0, 0, 0} };
 		test(s1, s2, 0);
@@ -189,15 +176,5 @@ void test_all()
 int main()
 {
 	test_all();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
